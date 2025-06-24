ea34317f0687decdfeba9e788b383b89f88bd89a
Jacopoo0 <jacopozucca05@gmail.com>
2025-06-24 15:26:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from transformers import pipeline
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templete")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+@app.post("/summary", response_class=HTMLResponse)
+def summary(request: Request, input_text: str = Form(...), service_type: str = Form(...) ):
+    if service_type == "summary":
+        generator = pipeline("summarization")
+        riassunto = generator(input_text)
+        return templates.TemplateResponse(request=request, name="summary.html", context={'summary': riassunto[0]["summary_text"]})
+
